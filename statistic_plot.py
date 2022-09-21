449958cb29c3305c8f1ea8beb8bf76bdee964db1
Luoxiaoyu828 <202108080011014@ctgu.edu.cn>
2022-09-21 17:09:53 +0800

    plot_item

--- statistic_plot.py
+++ statistic_plot.py
@@ -6,6 +6,7 @@ import scipy
 import matplotlib.pyplot as plt
 import os
 import matplotlib.ticker as mticker
+import plot_distribution as plt_d
 
 
 plt.rcParams['xtick.direction'] = 'in'
@@ -39,17 +40,16 @@ def m_vir(x, y, pixs, d):
 
 
 def get_outcat_all():
-    R2_data_params_path = r'R2_data_params'
-    file_list = [os.path.join(R2_data_params_path, item + '\MWISP_outcat.csv') for item in
-                 os.listdir(R2_data_params_path)]
+    R2_data_params_path = r'F:\Parameter_reduction\calc_mass\R2_data_params_fwhm'
+    file_list = [r'F:\Parameter_reduction\calc_mass\R2_data_params_fwhm\%s\MWISP_outcat_d_physical_again.csv' % item
+                 for item in os.listdir(R2_data_params_path)]
     outcat_all = pd.DataFrame([])
     for item in file_list:
-        if not os.path.exists(item):
-            continue
+
         print(item)
         outcat_item = pd.read_csv(item, sep='\t')
         outcat_all = pd.concat([outcat_all, outcat_item])
-    outcat_all.to_csv('outcat_all_d.csv', sep='\t', index=False)
+    outcat_all.to_csv('outcat_all_fwhm.csv', sep='\t', index=False)
 
 
 def plot_n(data):
@@ -93,7 +93,7 @@ def plot_n(data):
     plt.close()
 
 
-def plot_item(data, fig_name=None, xlabel=r'Tex $[K]$', rect=None, bins=25, uint='K', xlims=None, fitting=True):
+def plot_item(data, d_min, d_max, fig_name=None, xlabel=r'Tex $[K]$', rect=None, bins=25, uint='K', xlims=None, fitting=True):
     """
     绘制参数的统计直方图
     """
@@ -111,6 +111,7 @@ def plot_item(data, fig_name=None, xlabel=r'Tex $[K]$', rect=None, bins=25, uint
 
     fig = plt.figure(figsize=(6, 4))
     ax = fig.add_axes(rect)
+    # ax.set_xscale('log')
     ax.hist(data, bins=bins, histtype='step')
     ax.set_ylabel('Number')
     ax.set_xlabel(xlabel)
@@ -121,6 +122,7 @@ def plot_item(data, fig_name=None, xlabel=r'Tex $[K]$', rect=None, bins=25, uint
     lns2 = ax.axvline(Median, label='Median={:1.2f} {}'.format(Median, uint), linestyle=':')
     lns3 = ax.axvline(Peak, label='Peak $\sim${:1.2f} {}'.format(Peak, uint), linestyle='-.')
     ax1 = ax.twinx()
+    lns0 = ax1.plot([], [], '.', label='distance=[%.2fpc, %.2fpc]' % (d_min, d_max))
     if fitting:
         fit_par_lognorm = scipy.stats.lognorm.fit(data)
         lognorm_dist_fitted = scipy.stats.lognorm(*fit_par_lognorm)
@@ -133,9 +135,9 @@ def plot_item(data, fig_name=None, xlabel=r'Tex $[K]$', rect=None, bins=25, uint
         ax1.set_ylabel('Frequency [%]')
 
         # added these three lines
-        lns = lns1 + [lns2] + [lns3]
+        lns = lns0 + lns1 + [lns2] + [lns3]
     else:
-        lns = [lns2] + [lns3]
+        lns = lns0 + [lns2] + [lns3]
     labs = [l.get_label() for l in lns]
     ax1.legend(lns, labs, loc='best')
     ax1.set_ylim(0, ax1.get_yticks().max())
@@ -207,6 +209,7 @@ def plot_vir_a(M_sum, M_vir, fig_name=None, rect=None, ):
     fig = plt.figure(figsize=(6, 4))
     ax = fig.add_axes(rect)
     ax.plot(x, y, 'r--', zorder=10)
+    ax.plot(x, x, 'b--', zorder=11)
     ax.set_xscale('log')
     ax.set_yscale('log')
     # M_sum = np.log10(M_sum)
@@ -245,77 +248,96 @@ def plot_dis_vel(Velocity, Distance, rect=None, fig_name=None):
 
 if __name__ == '__main__':
     # get_outcat_all()
-    outcat_all1 = pd.read_csv('outcat_all.csv', sep='\t')
-    d_min = 1.5
-    d_max = 2.5
-    outcat_all = outcat_all1.loc[outcat_all1['Distance(kpc)'] > d_min]
-    outcat_all = outcat_all.loc[outcat_all['Distance(kpc)'] < d_max]
+    outcat_all1 = pd.read_csv('outcat_all_fwhm_dloc.csv', sep='\t')
+    d_min = 1.95
+    d_max = 2.2
+    save_path = 'Distance_%.2fto%.2f_3' % (d_min, d_max)
+    dis_key = 'Distance(kpc)_new'
+    outcat_all = outcat_all1.loc[outcat_all1[dis_key] > d_min]
+    outcat_all = outcat_all.loc[outcat_all[dis_key] < d_max]
+
+    Distance = outcat_all[dis_key].values
+
     outcat_all = outcat_all.loc[outcat_all1['reff(pc)'] < 3]
     outcat_all = outcat_all.loc[outcat_all1['v_fwhm(km/s)'] < 3]
 
-    save_path = 'Distance_%.1fto%.1f_1' %(d_min, d_max)
     os.makedirs(save_path, exist_ok=True)
     outcat_all = outcat_all[outcat_all['reff(pc)'].notna()]
 
     tex_max = outcat_all['Tex_max(K)'].values
     tex_mean = outcat_all['Tex_mean(K)'].values
 
-    plot_item(tex_max, xlabel=r'Tex max $[K]$', fig_name=os.path.join(save_path, 'tex_max.png'), bins=50)
-    plot_item(tex_mean, xlabel=r'Tex mean $[K]$', fig_name=os.path.join(save_path, 'tex_mean.png'), bins=50)
+    plot_item(tex_max, d_min, d_max, xlabel=r'Tex max $[K]$', fig_name=os.path.join(save_path, 'tex_max.png'), bins=50)
+    plot_item(tex_mean, d_min, d_max, xlabel=r'Tex mean $[K]$', fig_name=os.path.join(save_path, 'tex_mean.png'), bins=50)
 
     tao_mean = outcat_all['tao_max(-)'].values
-    plot_item(tao_mean, xlabel=r'$\tau$', rect=[0.1, 0.1, 0.8, 0.88], uint='', bins=50, xlims=[0, 2],
+    plot_item(tao_mean, d_min, d_max, xlabel=r'$\tau$', rect=[0.1, 0.1, 0.8, 0.88], uint='', bins=50, xlims=[0, 2],
               fig_name=os.path.join(save_path, 'tao_mean.png'))
 
     v_fwhm = outcat_all['v_fwhm(km/s)'].values
-    plot_item(v_fwhm, xlabel=r'$\Delta v_{FWHM}$ [km s$^{-1}$]', uint='km s$^{-1}$', rect=[0.08, 0.11, 0.85, 0.87],
+    plot_item(v_fwhm, d_min, d_max, xlabel=r'$\Delta v_{FWHM}$ [km s$^{-1}$]', uint='km s$^{-1}$', rect=[0.08, 0.11, 0.85, 0.87],
               fig_name=os.path.join(save_path, 'v_fwhm.png'))
     nh2_mean = outcat_all['nh2_mean(10**21*cm-2)'].values
-    plot_item(nh2_mean, xlabel=r'N(H$_2$) [$\times$10$^{21}$ cm$^{-2}$]', uint=r'$\times$10$^{21}$ cm$^{-2}$',
+    plot_item(nh2_mean, d_min, d_max, xlabel=r'N(H$_2$) [$\times$10$^{21}$ cm$^{-2}$]', uint=r'$\times$10$^{21}$ cm$^{-2}$',
               rect=[0.08, 0.11, 0.85, 0.87], xlims=[0, 10], bins=100,
               fig_name=os.path.join(save_path, 'nh2_mean.png'))
 
     nh2_max = outcat_all['nh2_max(10**21*cm-2)'].values
-    plot_item(nh2_max, xlabel=r'N(H$_2$) [$\times$10$^{21}$ cm$^{-2}$]', uint=r'$\times$10$^{21}$ cm$^{-2}$',
-              rect=[0.09, 0.11, 0.84, 0.87], bins=200, xlims=[0, 7],
+    plot_item(nh2_max, d_min, d_max, xlabel=r'N(H$_2$) [$\times$10$^{21}$ cm$^{-2}$]', uint=r'$\times$10$^{21}$ cm$^{-2}$',
+              rect=[0.09, 0.11, 0.84, 0.87], bins=200, xlims=[0, 100],
               fig_name=os.path.join(save_path, 'nh2_max.png'))
 
     reff = outcat_all['reff(pc)'].values
     reff[np.isnan(reff)] = 0
     a = outcat_all.loc[np.isnan(reff)]
-    plot_item(reff, xlabel=r'Reff [pc]', uint=r'pc', rect=[0.09, 0.11, 0.84, 0.87], bins=50, xlims=[0, 10],
+    plot_item(reff, d_min, d_max, xlabel=r'Reff [pc]', uint=r'pc', rect=[0.09, 0.11, 0.84, 0.87], bins=50, xlims=[0, 10],
               fig_name=os.path.join(save_path, 'reff.png'))
 
-    M_sum = outcat_all['M_sum(Msun)'].values
+    M_sum = outcat_all['M_sum(Msun)'].values / 2
     M_sum[np.isnan(M_sum)] = 0
-    plot_item(np.log10(M_sum), xlabel=r'$M_{LTE}$ $[M_{\odot}]$', uint=r'$M_{\odot}$', rect=[0.09, 0.11, 0.84, 0.87],
+    plot_item(np.log10(M_sum), d_min, d_max, xlabel=r'$M_{LTE}$ $[M_{\odot}]$', uint=r'$M_{\odot}$', rect=[0.09, 0.11, 0.84, 0.87],
               bins=50, fig_name=os.path.join(save_path, 'M_lte.png')
               )
 
-    M_vir = outcat_all['M_vir(Msun)'].values
-    plot_item(np.log10(M_vir), xlabel=r'$M_{vir}$ $[M_{\odot}]$', uint=r'$M_{\odot}$', rect=[0.09, 0.11, 0.84, 0.87],
+    M_vir = outcat_all['M_vir(Msun)'].values / (2.3548 ** 2) / 209 * 232.5
+    plot_item(np.log10(M_vir), d_min, d_max, xlabel=r'$M_{vir}$ $[M_{\odot}]$', uint=r'$M_{\odot}$', rect=[0.09, 0.11, 0.84, 0.87],
               bins=50, fig_name=os.path.join(save_path, 'M_vir.png'))
 
     vir_a = outcat_all['vir_a(-)'].values
     vir_a[np.isnan(vir_a)] = 0
-    plot_item(vir_a, xlabel=r'vir_a [-]', uint=r'-', rect=[0.09, 0.11, 0.84, 0.87], bins=100,
+    plot_item(vir_a, d_min, d_max, xlabel=r'vir_a [-]', uint=r'-', rect=[0.09, 0.11, 0.84, 0.87], bins=100,
               xlims=[0, 10], fig_name=os.path.join(save_path, 'vir_a.png'), fitting=False)
 
     Velocity = outcat_all['Velocity'].values
-    plot_item(Velocity, xlabel=r'Velocity', uint=r'km s$^{-1}$', rect=[0.09, 0.11, 0.84, 0.87], bins=50,
+    plot_item(Velocity, d_min, d_max, xlabel=r'Velocity', uint=r'km s$^{-1}$', rect=[0.09, 0.11, 0.84, 0.87], bins=50,
              fitting=False, fig_name=os.path.join(save_path, 'Velocity.png'))
 
     Distance = outcat_all['Distance(kpc)'].values
-    plot_item(Distance, xlabel=r'Distance [kpc]', uint=r'kpc', rect=[0.09, 0.11, 0.84, 0.87], bins=30,
+    plot_item(Distance, d_min, d_max, xlabel=r'Distance [kpc]', uint=r'kpc', rect=[0.09, 0.11, 0.84, 0.87], bins=30,
               fitting=False, fig_name=os.path.join(save_path, 'Distance.png'))
 
     plot_dis_vel(Velocity, Distance, rect=[0.09, 0.11, 0.84, 0.87],
                  fig_name=os.path.join(save_path, 'Velocity_Distance.png'))
 
     z = outcat_all['z(pc)'].values
-    plot_item(z, xlabel=r'z [pc]', uint=r'pc', rect=[0.09, 0.11, 0.84, 0.87], bins=50, fitting=False,
+    plot_item(z, d_min, d_max, xlabel=r'z [pc]', uint=r'pc', rect=[0.09, 0.11, 0.84, 0.87], bins=50, fitting=False,
               fig_name=os.path.join(save_path, 'z.png'))
 
     plot_vir_a(M_sum, M_vir, rect=[0.11, 0.14, 0.84, 0.85], fig_name=os.path.join(save_path, 'M_lte_to_M_vir.png'))
-
+    Galactic_Longitude = outcat_all['Galactic_Longitude'].values
+    Galactic_Latitude = outcat_all['Galactic_Latitude'].values
+    Velocity = outcat_all['Velocity'].values
     # plot_vir_a(M_sum, M_vir, rect=[0.11, 0.14, 0.84, 0.85])
+    plt_d.make_plot_l_v(Galactic_Longitude, Velocity, fig_name=os.path.join(save_path, 'l_v.png'))
+    plt_d.make_plot_l_b(Galactic_Longitude, Galactic_Latitude, Velocity, fig_name=os.path.join(save_path, 'l_b.png'))
+
+    # dinance_new = pd.read_csv(r'ccd_loc/outcat.txt', delim_whitespace=True)
+    # dis_new = dinance_new['(kpc1)'].values
+    #
+    # dis_old = outcat_all1['Distance(kpc)'].values
+    # plt.figure()
+    # plt.plot(dis_new, dis_old, '.')
+    #
+    # import astropy.io.fits as fits
+    # nh2 = fits.getdata(r'check_CDC/NH2_13CO.fits')
+    #
